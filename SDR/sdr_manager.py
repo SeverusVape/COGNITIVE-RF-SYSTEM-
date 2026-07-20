06c7f1c88f583c4be3641810bf56e78afbf2f055
Sergei Koshelev <sergeikoshelev@Sergeis-MacBook-Air.local>
2026-07-19 20:00:34 -0400

    Remove duplicate SDRManager definition

--- SDR/sdr_manager.py
+++ SDR/sdr_manager.py
@@ -10,47 +10,6 @@ class SDRManager:
         gain
     ):
 
-        self.sdr = RtlSdr()
-
-        self.sdr.sample_rate = sample_rate
-        self.sdr.center_freq = center_freq
-        self.sdr.gain = gain
-
-        print("SDR CONNECTED")
-
-    def read_samples(
-        self,
-        num_samples
-    ):
-
-        return self.sdr.read_samples(
-            num_samples
-        )
-
-    def tune(
-        self,
-        freq_hz
-    ):
-
-        self.sdr.center_freq = freq_hz
-
-    def close(self):
-
-        self.sdr.close()
-
-        print("SDR CLOSED")
-from rtlsdr import RtlSdr
-
-
-class SDRManager:
-
-    def __init__(
-        self,
-        sample_rate,
-        center_freq,
-        gain
-    ):
-
         self.sdr = None
         self.connected = False
 
